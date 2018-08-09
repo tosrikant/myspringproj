42a5436fef75bd9071adc7eb29c85b8b52d01a35
tosrikant <srikant.sse@gmail.com>
2018-08-09 22:03:57 +0530

    MyTestProj

---
+++ MyMavenProj/src/main/java/com/sritest/app/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.sritest.app;
+
+public class HelloWorld{
+
+public static void main(String[] args) {
+
+	System.out.println("Hello World");	
+
+
+}
+}
---
+++ MyMavenProj/src/test/java/com/sritest/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.sritest.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void test() {
+		
+	}
+
+}
